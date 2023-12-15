e299a3ebd198077560204d11207de413ec95f60b
mmihye <smilehye910@naver.com>
2023-12-15 12:40:53 +0900

    [#8] 7차 세미나

---
+++ src/main/java/com/example/seminar/controller/PostController.java
@@ -0,0 +1,26 @@
+package com.example.seminar.controller;
+
+
+import com.example.seminar.dto.PostRequest;
+import com.example.seminar.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+ㅇ
+    @PostMapping
+    public ResponseEntity<Void> createPost(@Valid @RequestBody PostRequest request) {
+        postService.create(request);
+        return ResponseEntity.ok().build();
+    }
+}
